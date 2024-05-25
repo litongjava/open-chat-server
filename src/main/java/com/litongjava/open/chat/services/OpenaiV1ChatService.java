4cb006ca5adb1c196121d83be9897d7b81594cfe
litongjava <litongjava@qq.com>
2024-05-24 15:18:23 -1000

    update OpenaiV1ChatHandler and OpenaiV1ChatService

--- src/main/java/com/litongjava/open/chat/services/OpenaiV1ChatService.java
+++ src/main/java/com/litongjava/open/chat/services/OpenaiV1ChatService.java
@@ -8,18 +8,23 @@ import lombok.extern.slf4j.Slf4j;
 public class OpenaiV1ChatService {
 
   public JSONObject beforeCompletions(JSONObject openAiRequestVo) {
+    // JSONArray jsonArray = openAiRequestVo.getJSONArray("messages");
     return openAiRequestVo;
   }
 
   public String processLine(String line) {
-    log.info("line:{}", line);
+//    log.info("line:{}", line);
     return line;
   }
 
-  public void complectionContent(StringBuffer complectionContent) {
-    
-    log.info("complectionContent:{}", complectionContent);
-    
+  public void completionContent(StringBuffer completionContent) {
+    log.info("completionContent:{}", completionContent);
+  }
+
+  public String functionCall(StringBuffer fnCallName, StringBuffer fnCallArgs) {
+    log.info("fn:{},{}", fnCallName.toString(), fnCallArgs.toString());
+    return null;
+
   }
 
 }
