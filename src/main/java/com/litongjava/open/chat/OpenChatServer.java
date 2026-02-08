5b40bd5c1c543ef9370d8bdfea1d447a25b095ca
litongjava <litongjava001@gmail.com>
2026-02-08 22:37:47 +0800

    @AComponentScan("com.litongjava.open.chat.controller")

--- src/main/java/com/litongjava/open/chat/OpenChatServer.java
+++ src/main/java/com/litongjava/open/chat/OpenChatServer.java
@@ -4,7 +4,7 @@ import com.litongjava.annotation.AComponentScan;
 import com.litongjava.open.chat.config.OpenChatServerConfig;
 import com.litongjava.tio.boot.TioApplication;
 
-@AComponentScan
+@AComponentScan("com.litongjava.open.chat.controller")
 public class OpenChatServer {
   public static void main(String[] args) {
     long start = System.currentTimeMillis();
